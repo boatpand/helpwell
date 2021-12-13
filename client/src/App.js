7fe4547016374d00312401e6db0a39fb238bc7fc
Namtanii <namtanm.01@gmail.com>
2021-12-13 19:54:27 +0700

    add Status to Request

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ import SignupVictim from "./signin/signupvictim";
 import HelperType from "./signin/helpertype";
 import Individual from "./signin/individual";
 import Organization from "./signin/organization";
-import Helps from "./victim/Help";
+import Help from "./victim/Help";
 import Summary from "./victim/Summary";
-<<<<<<< HEAD
+
 import SignupVictim2 from './signin/signupvictim2';
 import SignupHelper2 from './signin/signuphelper2';
 import VictimOtp from './signin/victim-otp';
@@ -21,19 +21,14 @@ import HelperProfile from './helper/helperprofile';
 import Admin from './admin/admin';
 import Forgetpass from './signin/forgetpass';
 import HelperEditProfile from './helper/helpereditprofile';
-=======
-import SignupVictim2 from "./signin/signupvictim2";
-import SignupHelper2 from "./signin/signuphelper2";
-import VictimOtp from "./signin/victim-otp";
-import HelperOtp from "./signin/helper-otp";
->>>>>>> master
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-<<<<<<< HEAD
+
           <Route exact path="/" component={Signin}/>
            <Route exact path="/helper"  component={Helper}/>
           <Route exact path="/accept-request/:id" component={EventDetail}/>
@@ -50,21 +45,7 @@ function App() {
           <Route exact path="/helpermap" component={HelperMap}/>
           <Route exact path="/helperprofile/:mobile" component={HelperProfile}/>
           <Route exact path="/helperprofile/edit-profile/:mobile" component={HelperEditProfile}/>
-=======
-          <Route exact path="/" component={Signin} />
-          <Route exact path="/helper" component={Helper} />
-          <Route exact path="/accept-request/:id" component={EventDetail} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/signup-victim" component={SignupVictim} />
-          <Route exact path="/signup-victim2" component={SignupVictim2} />
-          <Route exact path="/victim-otp" component={VictimOtp} />
-          <Route exact path="/helpertype" component={HelperType} />
-          <Route exact path="/individual" component={Individual} />
-          <Route exact path="/organization" component={Organization} />
-          <Route exact path="/signup-helper2" component={SignupHelper2} />
-          <Route exact path="/helper-otp" component={HelperOtp} />
->>>>>>> master
-          <Route exact path="/victims" component={Helps} />
+          <Route exact path="/victims" component={Help} />
           <Route exact path="/victims/summary" component={Summary} />
           <Route exact path="/admin" component={Admin}/>
         </Switch>
